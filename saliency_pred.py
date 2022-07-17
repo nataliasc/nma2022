7e8cda109ec8d0ec50c8ebaf9b857c50d3055b6a
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-07-17 17:11:16 +0200

    create implementation structure

---
+++ saliency_pred.py
@@ -0,0 +1,42 @@
+"""
+This file contains the code for saliency prediction algorithm.
+Creator: Lucy, Kaitlyn, Maria, Linas
+"""
+
+
+#################
+# data preprocessing
+#################
+
+
+
+#################
+# data set preparation
+#################
+
+
+
+
+#################
+# network class
+#################
+
+
+
+
+#################
+# training loop
+#################
+
+
+
+
+#################
+# evaluation function
+#################
+
+
+
+
+
+
