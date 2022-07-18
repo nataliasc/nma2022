1ce5df6ec33a94b7de86bea0a1fb71bd1eec4130
Natalia Scharfenberg <nscharfenber@uos.de>
2022-07-18 22:11:41 +0200

    Update model

--- model.py
+++ model.py
@@ -6,36 +6,39 @@ import random
 class DQN(nn.Module):
     def __init__(self, env, learning_rate=1e-3, **kwargs):
         super().__init__(**kwargs)
-        self.input_shape = env.observation_space.shape[:1]
         self.output_shape = env.action_space.n
 
 
-        self.conv1 = nn.Conv2d(in_channels=210*160, # for grayscale images?
+        self.conv1 = nn.Conv2d(in_channels=1,
                                out_channels=32, kernel_size=8,
-                               padding=2,
                                stride=4)
-        self.bn1 = nn.BatchNorm2d(32)
+
         self.conv2 = nn.Conv2d(32, 64, 4,
-                               padding=2, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
+                               stride=2)
+
         self.conv3 = nn.Conv2d(64, 64, 3,
-                               padding=2, stride=2)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.lin1 = nn.Linear(64, 512)
-        self.lin2 = nn.Linear(512, self.output_shape)
+                               stride=1)
+
+        self.fc1 = nn.Linear(in_features= 7 * 7 * 64, out_features=1024)
+
+        self.fc2 = nn.Linear(1024, self.output_shape)
 
     def forward(self, x):
         x = F.leaky_relu(self.conv1(x), 0.01)
         x = F.leaky_relu(self.conv2(x), 0.01)
         x = F.leaky_relu(self.conv3(x), 0.01)
-        x = F.leaky_relu(self.lin1(x), 0.01)
-        return self.lin2(x)
+        x = torch.flatten(x)
+        x = F.leaky_relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 if __name__ == '__main__':
         import gym
-        env = gym.make("ALE/Breakout-v5", obs_type="grayscale")
+        from gym.wrappers import AtariPreprocessing
+        env = gym.make("ALE/Breakout-v5", frameskip=1)
+        env = AtariPreprocessing(env, frame_skip=4, new_step_api=True)
         env.reset()
         action = random.randrange(env.action_space.n)
-        obs, reward, done, info = env.step(action)
+        obs, reward, done, terminal, info = env.step(action) # why both done and terminal?
         model = DQN(env)
-        print(model(action))
+        model(torch.Tensor(obs).unsqueeze(0)) # input shape is now (1, 84, 84)
