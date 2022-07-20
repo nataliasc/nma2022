5722fdcce9f40410f16f498d1c0aef4d433d950e
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-07-20 16:15:48 +0200

    frame crop

--- data_preprocessing_gaze.py
+++ data_preprocessing_gaze.py
@@ -8,70 +8,32 @@ output: n_samples*(4_frame_episodes, 1_saliency_density_map)
 # load meta data to get subject num
 import numpy as np
 import pandas as pd
+from preprop_util import *
 from glob import glob
 import os
 import cv2
+import matplotlib.pyplot as plt
 
 metadata = pd.read_csv(r'raw_data/meta_data.csv')
 metadata_breakout = metadata[metadata['GameName'] == 'breakout']
 print(metadata_breakout.head())
 
-
 # %%
-# load frame dir and gaze txt for subject
-def read_txt_data(txt_path, time_frame):
-    '''
-    read data from txt file containing gaze pos
-    :param txt_path: file path to txt file
-    :param time_frame: how much ms of gaze pos to get from file, per 50ms is 100 gaze positions
-    :return: list of lists containing data values per line  shape: n_frame*time_frame
-    '''
-    with open(txt_path, 'r') as filestream:
-        num_datapoints = time_frame * 2 + 6  # first couple of colums plus taken gaze pos
-        all_lines = []
-        for line in filestream:
-            currentline = line.split(",")
-            if len(currentline) > num_datapoints:
-                all_lines.append(currentline[:num_datapoints])
-            else:
-                filler = np.full(num_datapoints - len(currentline), np.NaN).tolist()
-                currentline += filler
-                all_lines.append(currentline)
-
-    return all_lines[1:]
-
-
-def load_frames_gazetxt(trial_id, metadataframe, data_path='raw_data/breakout/'):
-    '''
-    given trial id load from raw_data/breakout corresponding frames and txt file for gaze
-    read frames as grey scale
-
-    :param data_path: where data for one game is stored
-    :param trial_id: label for which dir to load
-    :param metadataframe: dataframe containing reference
-    :return: all grey scaled game frames, list of gaze data
-    '''
-
-    trial_subject_key = str(trial_id) + '_' + metadataframe[metadataframe['trial_id'] == trial_id]['subject_id'].item()
-    trial_dir = glob(os.path.join(data_path, trial_subject_key + '*'))  # contain txt file and dir for frames
-    # read data from txt file
-    gaze_data = read_txt_data(trial_dir[0], 50)
-    # load frames from frame fir
-    frames_grey = []
-    frame_glob = glob(os.path.join(trial_dir[1] + '/', '*.png'))
-    for path in frame_glob:
-        grey = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-        frames_grey.append(grey)
-
-    assert len(frames_grey) == len(gaze_data)
-
-    return frames_grey, gaze_data
-
-
+# get ordered game frames and corresponding gaze data from data files
 all_grey_frames, gaze_data = load_frames_gazetxt(58, metadata_breakout)
 
 # %%
-# get data from txt file
+# crop all grey frames
+img_w = 84
+img_h = 84
+
+cropped_frames = crop_frame(all_grey_frames, img_w, img_h)
+
+# %%
+for i in range(10):
+    plt.imshow(cropped_frames[i])
+    plt.show()
+# %%
 # turn gaze position from str(num.num) to list[num, num]
 def gaze_str_to_intlist(gaze_pos):
     '''
