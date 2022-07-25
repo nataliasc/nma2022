a6d40a45efd169e96abdfb4ffeaf37415b50e75d
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-07-25 20:26:41 +0100

    try adamw

--- saliency_pred.py
+++ saliency_pred.py
@@ -38,10 +38,22 @@ train_set, val_set, test_set = data.random_split(dataset, split, generator=torch
 wandb.login(key='25f10546ef384a6f1ab9446b42d7513024dea001')
 wandb.init(project="saliency-prediction", entity="nma2022")
 
+sweep_config = {
+    'method': 'bayes'
+    }
+
+metric = {
+    'name': 'val_loss',
+    'goal': 'minimize'
+    }
+
+sweep_config['metric'] = metric
+
+
 config = wandb.config
 config.batch_size = 100
 config.lr = 1e-3
-config.epoch = 500
+config.epoch = 400
 config.log_freq = 200
 config.val_freq = 200
 
@@ -57,7 +69,8 @@ net = SimpleFCN(config.batch_size, DEVICE)
 net.float().to(DEVICE)
 #criterion = nn.KLDivLoss(reduction="batchmean", log_target=True)
 criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=config.lr)
+# optimizer = torch.optim.Adam(net.parameters(), lr=config.lr)
+optimizer = torch.optim.AdamW(net.parameters(), lr=config.lr)
 
 wandb.watch(net, log_freq=100)
 
