e633036a3777f58348437082ab0303a443c42f14
lucyzmf <67420952+lucyzmf@users.noreply.github.com>
2022-07-28 13:59:40 +0100

    debug

--- saliency_wrapper.py
+++ saliency_wrapper.py
@@ -65,7 +65,7 @@ class SaliencyMap4F(gym.ObservationWrapper):
         # call to generate saliency map here
         frames = torch.tensor(frames).to(self.net_device)
         frames = torch.unsqueeze(frames, dim=0).float()  # add batch dim
-        saliency_map = torch.squeeze(self.s_map(frames)).cpu().numpy()
+        saliency_map = torch.squeeze(self.s_map(frames)).detach().cpu().numpy()
         frame_stack = []
         for i in range(frames.shape[0]):
             product = np.multiply(frames[i], saliency_map)
